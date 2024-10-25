7dcf22e3f3c3ef4f79c4b254fb8636c9d4f3d664
Gerald <geraldjoly191@gmail.com>
2024-10-25 10:19:13 -0300

    Agreando las clase de manejo de los datos de los formulario

---
+++ Datos/CEmpleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_de_empleados.Datos
+{
+    public class CEmpleado
+    {
+
+        public CEmpleado() { }
+        public CEmpleado(string id, string lastName, string firstName, uint age, string email)
+        {
+            Id = id;
+            LastName = lastName;
+            FirstName = firstName;
+            Age = age;
+            Email = email;
+        }
+
+        public string Id {  get; private set; }
+        public string LastName {  get; private set; }
+        public string FirstName { get; private set;}
+        public uint Age { get; private set; }
+        public string Email { get; private set; }
+    }
+}
---
+++ Datos/CEmpleadoRepositorio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Registro_de_empleados.Datos
+{
+    public static class CEmpleadoRepositorio
+    {
+        //private const string rutaArchivoJSON = @"\Datos.json";
+
+        public static List<CEmpleado> listaDeEmpleados { get; private set; }
+        
+        public static void InicalizarRepositorio()
+        {
+                listaDeEmpleados = new List<CEmpleado>();        
+        }
+        public static void AgregarEmpleado(CEmpleado empleado)
+        {
+            listaDeEmpleados.Add(empleado);
+            string json= JsonSerializer.Serialize(listaDeEmpleados);
+            File.WriteAllText("Datos.json", json);
+        }
+
+        public static void EliminarEmpleado(string id)
+        {
+            listaDeEmpleados.RemoveAll(e => e.Id.Equals(id));
+            string json = JsonSerializer.Serialize(listaDeEmpleados);
+            File.WriteAllText("Datos.json", json);
+        }
+
+        public static void  ActuatlizarEmpleado(string IdEmpleado,CEmpleado empleadoModificado)
+        {
+           
+            int indiceEmpleadoOriginal = listaDeEmpleados.FindIndex(e=> e.Id == IdEmpleado);
+            if(indiceEmpleadoOriginal != -1)
+            {
+                listaDeEmpleados[indiceEmpleadoOriginal]= empleadoModificado;
+                string json = JsonSerializer.Serialize(listaDeEmpleados);
+                File.WriteAllText("Datos.json", json);
+            } 
+        }
+
+    }
+}
